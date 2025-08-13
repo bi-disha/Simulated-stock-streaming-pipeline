96835d177c8b332a92ce69b251d1ab0fd67423fe
Rohit Gomes <gomesrohit92@gmail.com>
2025-08-13 21:11:43 +0530

    Updated Data Load query
    Solved a syntax error

--- data_warehouse/utils/data_load.py
+++ data_warehouse/utils/data_load.py
@@ -7,6 +7,13 @@ def load_data(data):
     for stock in data.keys():
         stock_data =data[stock]
         
-        query =f"INSERT INTO {stock} (ticker, price, volume, buy_pressure, timestamp) VALUES ({stock_data["price"]}, {stock_data["volume"]}, {stock_data["buy_pressure"]}, {stock_data["timestamp"]})"
-        cursor.execute(query)
+        query =f"INSERT INTO {stock} (ticker, price, volume, buy_pressure, timestamp) VALUES (?, ?, ?, ?, ?)"
+        data_to_insert = (
+        stock_data['ticker'],
+        stock_data['price'],
+        stock_data['volume'],
+        stock_data['buy_pressure'],
+        stock_data['timestamp']
+    )
+        cursor.execute(query,data_to_insert)
     conn.commit()
